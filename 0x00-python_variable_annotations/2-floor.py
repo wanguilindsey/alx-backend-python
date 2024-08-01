38b734e9e136a0c2a4c70b10b410d8001292c899
wanguilindsey <wanguilindsey@gmail.com>
2024-08-01 13:38:48 -0700

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - floor
+
+This module contains a function `floor` that takes a float argument and
+returns the floor value as an integer.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float as an integer.
+
+    Args:
+        n (float): The float number to be floored.
+
+    Returns:
+        int: The floor value of n.
+    """
+    return int(n)
