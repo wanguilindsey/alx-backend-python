efef90f467441782dc2bf11121cd9fb9e1c09933
wanguilindsey <wanguilindsey@gmail.com>
2024-08-01 13:48:22 -0700

    variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define and annotate variables with specified values.
+
+This module defines several variables with specific types and values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
