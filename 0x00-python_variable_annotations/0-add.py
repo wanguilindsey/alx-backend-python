e5935c455757bfaa8ceaef61dafce4a39d802605
wanguilindsey <wanguilindsey@gmail.com>
2024-08-01 13:31:35 -0700

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Type-Annotated Function: add
+
+This module contains a function `add` that takes two float values as input
+and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two float numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
